815c45f179fb5ffc50f3e6f109059e8f019f56c3
lizhihua <275091674@qq.com>
2017-11-18 22:35:04 +0800

    webpack.js.org/guides/shimming

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ function component() {
 
   var element = document.createElement('div');
 
+//  const join = function (array, str) {
+//    return array.join(str)
+//  }
+
   element.innerHTML = join(['Hello', 'webpack'], ' ');
 
   return element;
