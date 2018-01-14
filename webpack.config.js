7cd61d21d12b6c190bf772ed9eefb8f0914e7609
lizhihua <275091674@qq.com>
2018-01-14 13:44:16 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/index.js',
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       exclude: ['index.html'],
       verbose: true,
       dry: false,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Progressive Web Application',
+    }),
+    new WorkboxPlugin({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
     })
   ],
   output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   }
-};
+}
